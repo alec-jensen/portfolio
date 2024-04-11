e7c1755dd87933d9a70d86f83ce48dd65188136d
Alec Jensen <alecbradyjensen@gmail.com>
2024-04-11 08:11:49 -0500

    fix kb nav

--- src/lib/KBNav.svelte
+++ src/lib/KBNav.svelte
@@ -1,14 +1,11 @@
 <script>
     import { goto } from '$app/navigation';
+    import { onMount } from 'svelte';
 
-    let is_modifier_key_held = false;
-
-    export function handleKeydown(event) {
-        if (event.key === 'Meta' || event.key === 'Control' || event.key === 'Alt' || event.key === 'Shift') {
-            is_modifier_key_held = true;
+    function handleKeydown(event) {
+        if (event.shiftKey || event.ctrlKey || event.altKey || event.metaKey) {
+            return;
         }
-
-        if (is_modifier_key_held) return;
 
         if (event.key === 'h') {
             goto('/');
@@ -20,12 +17,6 @@
             goto('/contact');
         }
     }
-
-    export function handleKeyup(event) {
-        if (event.key === 'Meta' || event.key === 'Control' || event.key === 'Alt' || event.key === 'Shift') {
-            is_modifier_key_held = false;
-        }
-    }
 </script>
 
-<svelte:window on:keydown={handleKeydown} on:keyup={handleKeyup} />
+<svelte:window on:keydown={handleKeydown} />
